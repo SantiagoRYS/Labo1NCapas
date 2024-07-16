4aeec183cf6d7075dd611f44f200cc9de70ec721
Santiago <Santiagorys2003@gmail.com>
2024-07-16 17:59:43 -0500

    feat(back-end): Add class EFReposiroty with method CreateAsync

--- Orders/DAL/IRepository.cs
+++ Orders/DAL/IRepository.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace DAL
 {
-    internal interface Interface1
+    namespace Models
     {
+        public interface IRepository : IDisposable
+        {
+            Task<TEntity> CreateAsync<TEntity>(TEntity toCreate) where TEntity : class;
+
+            Task<bool> DeleteAsync<TEntity>(TEntity toDelete) where TEntity : class;
+
+            Task<bool> UpdateAsync<TEntity>(TEntity toUpdate) where TEntity : class;
+
+            Task<TEntity> RetreiveAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+
+            Task<List<TEntity>> FilterAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+        }
     }
+
 }
