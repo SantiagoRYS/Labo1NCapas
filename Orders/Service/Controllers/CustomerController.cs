176ac4a593571dca62108fe7bd7ff2c8e2fda3b8
SantiagoRYS <152504184+SantiagoRYS@users.noreply.github.com>
2024-07-24 21:06:00 -0500

    feat(back-end): Add GetAll method

--- Orders/Service/Controllers/CustomerController.cs
+++ Orders/Service/Controllers/CustomerController.cs
@@ -1,11 +1,61 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using BLL; 
+using Entities.Models; 
+using Microsoft.AspNetCore.Mvc; 
+using SLC; 
+using System.Linq.Expressions; 
+using Microsoft.AspNetCore.Http; 
+using BLL.Exceptions;
 
-namespace Service.Controllers
+namespace Services.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CustomerController : ControllerBase
+    public class CustomerController : ControllerBase, ICustomerService
     {
+        private readonly Customers _bll;
+
+        public CustomerController(Customers bll)
+        {
+            _bll = bll;
+        }
+
+        public Task<ActionResult<Customer>> CreateAsync([FromBody] Customer toCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ActionResult<List<Customer>>> GetAll()
+        {
+            try
+            {
+                var result = await _bll.RetrieveAllAsync();
+                return Ok(result);
+            }
+            catch (CustomerExceptions ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error ocurred");
+            }
+        }
+
+        public Task<ActionResult<Customer>> RetrieveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult> UpdateAsync(int id, [FromBody] Customer toUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
