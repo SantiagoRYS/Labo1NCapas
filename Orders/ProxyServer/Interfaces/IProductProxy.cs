ccb184303aeae609c4f4370c849ff2846dd5178d
SantiagoRYS <152504184+SantiagoRYS@users.noreply.github.com>
2024-08-04 15:43:16 -0500

    feat(back-end): Add methods in IProductProxy

--- Orders/ProxyServer/Interfaces/IProductProxy.cs
+++ Orders/ProxyServer/Interfaces/IProductProxy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace ProxyServer.Interfaces
 {
-    public interface Interface1
+    public interface IProductProxy
     {
+        Task<Product> CreateAsync(Product product);
+        Task<bool> DeleteAsync(int id);
+        Task<List<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<bool> UpdateAsync(int id, Product product);
+
     }
 }
